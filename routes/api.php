8184219bf342ae3e91ff61ac787dec699abe46f4
Gašper Bohorč <gasperb@marand.si>
2024-09-05 13:05:30 +0200

    api

--- routes/api.php
+++ routes/api.php
@@ -1,20 +1,13 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EventsController;
 
 
-// Route to get all users
-Route::get('/users', function () {
-    return response()->json([
-        'users' => App\Models\User::all()
-    ]);
-});
-
-// Route to test the Laravel setup
-Route::get('/test', function () {
-    return 'I am from Laravel';
-});
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
 
 Route::get('events', [EventsController::class, 'getEvents']);
-Route::post('events', [EventsController::class, 'storeEvent']);
+Route::post('event', [EventsController::class, 'storeEvent']);
