fa6e7752078b647dd20eae57b96c637f5a65d68b
Gašper Bohorč <gasperb@marand.si>
2024-09-05 23:16:46 +0200

    vue

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -2,7 +2,7 @@
 <template>
   <div id="app">
     <header>
-      <h1>Welcome to My Vue App</h1>
+      <h1>Dewesoft google app</h1>
     </header>
     <main>
       <event-list :events="events"></event-list>
