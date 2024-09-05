f5b4439a57f93806293412010fef7b10a2674210
Gašper Bohorč <gasperb@marand.si>
2024-09-05 22:51:16 +0200

    vue added

---
+++ resources/js/App.vue
@@ -0,0 +1,26 @@
+<!-- resources/js/components/App.vue -->
+<template>
+  <div id="app">
+    <header>
+      <h1>Welcome to My Vue App</h1>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style scoped>
+#app {
+  text-align: center;
+  font-family: Arial, sans-serif;
+}
+header {
+  background: #42b983;
+  padding: 1em;
+  color: white;
+}
+</style>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,6 @@
-import './bootstrap';
+// resources/js/app.js
+import { createApp } from 'vue';
+import App from './App.vue';
+
+const app = createApp(App);
+app.mount('#app');
