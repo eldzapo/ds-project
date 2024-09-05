c9d10cf18a824af2c5c9f4395b3f6b4f4471b2c4
Gašper Bohorč <gasperb@marand.si>
2024-09-05 22:57:18 +0200

    vue

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -4,12 +4,37 @@
     <header>
       <h1>Welcome to My Vue App</h1>
     </header>
+    <main>
+      <example-component></example-component>
+      <button @click="fetchEvents">Fetch Events</button>
+    </main>
   </div>
 </template>
 
 <script>
+import ExampleComponent from './components/ExampleComponent.vue';
+import { ref } from 'vue';
+
 export default {
   name: 'App',
+  components: {
+    ExampleComponent,
+  },
+  setup() {
+    const fetchEvents = async () => {
+      try {
+        const response = await fetch('http://localhost:8000/api/events');
+        const data = await response.json();
+        console.log(data.events);
+      } catch (error) {
+        console.error('Error fetching events:', error);
+      }
+    };
+
+    return {
+      fetchEvents,
+    };
+  },
 };
 </script>
 
@@ -22,5 +47,18 @@ header {
   background: #42b983;
   padding: 1em;
   color: white;
+}
+button {
+  margin-top: 1em;
+  padding: 0.5em 1em;
+  font-size: 1em;
+  color: white;
+  background-color: #42b983;
+  border: none;
+  border-radius: 0.3em;
+  cursor: pointer;
+}
+button:hover {
+  background-color: #36a17a;
 }
 </style>
